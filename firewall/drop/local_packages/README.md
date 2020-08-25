e6756e47a52a21f8a727af708a7d68a341557645
Everton Segur <everton.segur@zup.com.br>
2020-08-25 17:50:03 -0300

    first commit

---
+++ firewall/drop/local_packages/README.md
@@ -0,0 +1,17 @@
+# Ritchie Formula
+
+## command
+
+Start now
+```bash
+echo "{}" | rit firewall drop local_packages --stdin
+```
+
+If you want to describe the network to be configured input the command bellow.
+```bash
+rit firewall drop local_packages
+```
+
+## description
+
+description of formula
---
+++ firewall/drop/local_packages/src/formula/formula.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+rule_message="Applied_from_Ritchie_CLI"
+
+function apply_rules() {
+  sudo iptables -I INPUT -s $NETWORK_ADDRESS -j DROP -m comment --comment $rule_message;
+}
+
+function apply_without_params() {
+
+  HOSTNAME=$(hostname -I | cut -d ' ' -f1);
+  PARSED_ADDRESS=$(echo $HOSTNAME | sed 's/\./io/g');
+  FIRST=$(echo $PARSED_ADDRESS | cut -d 'i' -f1 );
+  SECOND=$(echo $PARSED_ADDRESS | cut -d 'i' -f2 | cut -d 'o' -f2 );
+  THIRD=$(echo $PARSED_ADDRESS | cut -d 'i' -f3 | cut -d 'o' -f2);
+
+  NETWORK_ADDRESS=$(echo "$(echo $FIRST).$(echo $SECOND).$(echo $THIRD)".0/24);
+
+  sudo iptables -I INPUT -s $NETWORK_ADDRESS -j DROP -m comment --comment $rule_message;
+}
+
+runFormula() {
+  if [[ $NETWORK_ADDRESS != "unnecessary" ]] 
+  then
+    apply_rules;
+  else
+    apply_without_params;
+  fi;
+}
